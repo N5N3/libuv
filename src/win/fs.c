4f92483c6c91e4b31db65ee18ef460666a507f40
Elliot Saba <staticfloat@gmail.com>
2023-05-23 14:30:23 +0000

    Define `S_IRWXU` and others if not already defined.

--- src/win/fs.c
+++ src/win/fs.c
@@ -44,6 +44,23 @@
 #define UV_FS_FREE_PTR           0x0008
 #define UV_FS_CLEANEDUP          0x0010
 
+#ifndef S_IRWXU
+#define S_IRWXU                  0000700    /* RWX mask for owner */
+#define S_IRUSR                  0000400    /* R for owner */
+#define S_IWUSR                  0000200    /* W for owner */
+#define S_IXUSR                  0000100    /* X for owner */
+
+#define S_IRWXG                  0000070    /* RWX mask for group */
+#define S_IRGRP                  0000040    /* R for group */
+#define S_IWGRP                  0000020    /* W for group */
+#define S_IXGRP                  0000010    /* X for group */
+
+#define S_IRWXO                  0000007    /* RWX mask for other */
+#define S_IROTH                  0000004    /* R for other */
+#define S_IWOTH                  0000002    /* W for other */
+#define S_IXOTH                  0000001    /* X for other */
+#endif
+
 /* number of attempts to generate a unique directory name before declaring failure */
 #define TMP_MAX 32767
 
