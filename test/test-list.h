b2384ab7d1952afc81b08e7e55f743f56bb5b306
Jameson Nash <vtjnash@gmail.com>
2025-02-05 18:13:23 +0000

    fixup! linux: don't delay EPOLL_CTL_DEL operations (#4328)
    Backporting mistake by @Keno from ca3a5a431a1c37859b6508e6b2a288092337029a. Please run tests.

--- test/test-list.h
+++ test/test-list.h
@@ -1202,28 +1202,8 @@ TASK_LIST_START
   TEST_ENTRY  (req_type_name)
   TEST_ENTRY  (getters_setters)
 
-<<<<<<< HEAD
-=======
-#ifndef _WIN32
-  TEST_ENTRY  (fork_timer)
-  TEST_ENTRY  (fork_socketpair)
-  TEST_ENTRY  (fork_socketpair_started)
-  TEST_ENTRY  (fork_signal_to_child)
-  TEST_ENTRY  (fork_signal_to_child_closed)
-  TEST_ENTRY  (fork_close_signal_in_child)
-#ifndef __APPLE__
-  TEST_ENTRY  (fork_fs_events_child)
-  TEST_ENTRY  (fork_fs_events_child_dir)
-  TEST_ENTRY  (fork_fs_events_file_parent_child)
-#endif
-#ifndef __MVS__
-  TEST_ENTRY  (fork_threadpool_queue_work_simple)
-#endif
-#endif
-
   TEST_ENTRY  (iouring_pollhup)
 
->>>>>>> 3ecce914... linux: don't delay EPOLL_CTL_DEL operations (#4328)
   TEST_ENTRY  (utf8_decode1)
   TEST_ENTRY  (utf8_decode1_overrun)
   TEST_ENTRY  (uname)
